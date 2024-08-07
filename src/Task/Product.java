c3e6aafa117d3bcbbe189836c7a1faa4865b391b
Balaji <balajie7373@gmail.com>
2024-08-07 17:26:45 +0530

    Pushing

---
+++ src/Task/Product.java
@@ -0,0 +1,18 @@
+package Task;
+
+import java.util.Scanner;
+
+public class Product {
+
+    int pid,qty;
+    float price;
+    public static void main(String[] args){
+        Scanner scanner=new Scanner(System.in);
+        int pid = scanner.nextInt();
+        System.out.println("Please enter the Pid: " + pid);
+
+
+    }
+
+
+}
